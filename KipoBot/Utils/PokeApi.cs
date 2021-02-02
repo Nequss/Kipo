56cd43a50ba602356c02ca75552db2794f26835e
OwczarekGit <OwczarekGit>
2021-02-02 16:01:13 +0100

    Added pokeApi

---
+++ KipoBot/Utils/PokeApi.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using PokeApiNet;
+    
+namespace Kipo.Utils
+{
+    public class PokeApi
+    {
+        class Pokemon
+        {
+            public String Name { get; set; }
+            
+        }
+        
+        public static async Task test(SocketCommandContext ctx, string name)
+        {
+            
+        }
+    }
+}
