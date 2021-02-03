ad843e03937ece59f48e6a4a9ae38650ac3a4d21
OwczarekGit <OwczarekGit>
2021-02-03 15:31:14 +0100

    Added method for asserting that paths exists and creating them if they dont.

--- KipoBot/Utils/Helpers.cs
+++ KipoBot/Utils/Helpers.cs
@@ -1,12 +1,16 @@
 using Discord.WebSocket;
 using Discord.Commands;
 using System;
+using System.IO;
 using System.Linq;
+using Discord;
 
 namespace KipoBot.Utils
 {
     public static class Helpers
     {
+        public static readonly String WORKING_DIRECTORY = Directory.GetCurrentDirectory();
+        
         public static SocketGuildUser extractUser(SocketCommandContext ctx, string message)
         {
             string impliedUser = message.TrimStart().TrimEnd();
@@ -34,6 +38,27 @@ namespace KipoBot.Utils
             }
 
             return null;
+        }
+
+        public static bool AssertPaths(String[] paths)
+        {
+            bool isSuccess = true;
+            foreach (var path in paths)
+            {
+                if (!Directory.Exists( $"{WORKING_DIRECTORY}/{path}"))
+                {
+                    try
+                    {
+                        Directory.CreateDirectory($"{WORKING_DIRECTORY}/{path}");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Failed to create directory: " + path + "\n" + e);
+                        isSuccess = false;
+                    }
+                }
+            }
+            return isSuccess;
         }
     }
 }
