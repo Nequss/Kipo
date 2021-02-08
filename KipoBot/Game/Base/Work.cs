e0371dcdb2ba49989a8fcaeb64091f80ef417a5a
OwczarekGit <OwczarekGit>
2021-02-08 21:36:47 +0100

    Modified work base class.

--- KipoBot/Game/Base/Work.cs
+++ KipoBot/Game/Base/Work.cs
@@ -9,6 +9,9 @@ namespace KipoBot.Game.Base
         public int reward;
         public String name;
         public byte energyCost;
+        public byte thirstCost;
+        public byte hungerCost;
+        public byte happinessCost;
         public byte timeDuration;
         public Pet worker;
         public Pet.Stage reqStage;
@@ -16,33 +19,19 @@ namespace KipoBot.Game.Base
         public DateTime timeStarted;
         public DateTime timeEnd;
         public Player workerOwner;
+        public SocketCommandContext context;
 
         protected Work(Pet pet, Player owner, SocketCommandContext ctx)
         {
             worker = pet;
             worker.currentWork = this;
             workerOwner = owner;
-            
-            if (!canWork())
-            {
-                ctx.Channel.SendMessageAsync($"This work requires your pet to be at least at stage: {reqStage}\nCome back when it gets older!");
-                return;
-            }
-            
-            if (reqItem != null && !hasReqItem())
-            {
-                ctx.Channel.SendMessageAsync($"This work requires an item that you currently don't have: {reqItem}\nCome back when you have it!");
-                //return;
-            }
-
-            workStarted();
-            ctx.Channel.SendMessageAsync($"{pet.name} started job: {name}");
-            workCompleted();
+            context = ctx;
         }
 
-        public bool canWork()
+        public bool isOldEnough()
         {
-            return worker.stage >= reqStage && hasReqItem();
+            return worker.stage >= reqStage;
         }
 
         public bool hasReqItem()
@@ -60,18 +49,55 @@ namespace KipoBot.Game.Base
 
         public void workCompleted()
         {
-            Console.WriteLine($"{worker.name} has finished!\n{workerOwner.id} had {workerOwner.wallet} money.");
+            context.Channel.SendMessageAsync($"{worker.name} has finished!\n{workerOwner.id} had {workerOwner.wallet} money.");
             worker.energy -= energyCost;
             workerOwner.wallet += reward;
             worker.currentWork = null;
-            Console.WriteLine($"Now has {workerOwner.wallet} money.");
+            context.Channel.SendMessageAsync($"Now has {workerOwner.wallet} money.");
         }
 
         public void workStarted()
         {
             timeStarted = DateTime.Now;
             timeEnd = timeStarted + new TimeSpan(timeDuration,0,0);
-            Console.WriteLine($"Work started at: {timeStarted}\nWill end at: {timeEnd}\nTime remaining: {timeEnd.Subtract(timeStarted)}");
+            context.Channel.SendMessageAsync($"Work started at: {timeStarted}\nWill end at: {timeEnd}\nTime remaining: {timeEnd.Subtract(timeStarted)}");
+        }
+
+        public bool satisfiesReqs()
+        {
+            if (worker.energy < energyCost)
+                return false;
+
+            if (worker.thirst < thirstCost)
+                return false;
+            
+            if (worker.hunger < hungerCost)
+                return false; 
+            
+            if (worker.hapiness < happinessCost)
+                return false;
+
+            return true;
+        }
+
+        public void verify()
+        {
+            if (!isOldEnough())
+            {
+                context.Channel.SendMessageAsync($"This work requires your pet to be at least at stage: {reqStage}\nCome back when it gets older!");
+                return;
+            }
+            
+            if (reqItem != null && !hasReqItem())
+            {
+                //context.Channel.SendMessageAsync($"This work requires an item that you currently don't have: {reqItem}\nCome back when you have it!");
+                //return;
+            }
+
+            satisfiesReqs();
+            workStarted();
+            context.Channel.SendMessageAsync($"{worker.name} started job: {name}");
+            workCompleted();
         }
     }
 }
