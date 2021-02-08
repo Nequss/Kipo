7ce9af06dfeb9359864ce065f7bd73f6fa526303
OwczarekGit <OwczarekGit>
2021-02-08 20:39:36 +0100

    Created Factory workplace.

--- KipoBot/Game/Jobs/Factory.cs
+++ KipoBot/Game/Jobs/Factory.cs
@@ -1,9 +1,18 @@
+using System.Runtime.CompilerServices;
+using Discord.Commands;
 using KipoBot.Game.Base;
 
-namespace KipoBot.Game.Works
+namespace KipoBot.Game.Jobs
 {
-    public class Factory : Work
+    class Factory : Work
     {
-        
+        public Factory(Pet pet, Player owner, SocketCommandContext ctx) : base(pet, owner, ctx)
+        {
+            name = "Factory";
+            reward = 25;
+            energyCost = 10;
+            timeDuration = 1;
+            reqStage = Pet.Stage.Teenager;
+        }
     }
 }
