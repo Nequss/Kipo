26efae1e36780a800776bb01eff6e7cf53b097f4
Nequss <snequs@gmail.com>
2021-02-08 20:44:52 +0100

    Added Potions

---
+++ KipoBot/Game/Items/Potions/BigPotion.cs
@@ -0,0 +1,37 @@
+﻿using Discord;
+using Discord.Webhook;
+using Discord.WebSocket;
+using Discord.Commands;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using KipoBot.Services;
+using KipoBot.Game.Base;
+
+namespace KipoBot.Game.Items.Toys
+{
+    class BigPotion : Item
+    {
+        public BigPotion()
+        {
+            type = Type.Potion;
+            price = 500;
+            name = "Big Potion";
+            describtion = "This somehow weirdly heals a lot of your pets wounds";
+            health = 100;
+        }
+
+        public override Pet Use(Pet pet)
+        {
+            return pet;
+        }
+    }
+}
