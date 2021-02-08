da50d0c7e62633af846c3a7ad9ba69fedf3f0b71
OwczarekGit <OwczarekGit>
2021-02-08 23:33:14 +0100

    Changed output to true or false.

--- KipoBot/Modules/GeneralModule.cs
+++ KipoBot/Modules/GeneralModule.cs
@@ -81,16 +81,12 @@ namespace KipoBot.Modules
                     author.WithName($"Pet Card | {player.active.type}");
                 });
 
-                String tmpWork = "None";
-                if (player.active.currentWork != null)
-                    tmpWork = player.active.currentWork.name;
-
                 embedBuilder.AddField("Pet info",
                     $"Name: {player.active.name}\n" +
                     $"Rarity: {player.active.rarity}\n" +
                     $"Level: {player.active.level}\n" +
                     $"Experience: {player.active.xp}\n" +
-                    $"Currnet Job: {tmpWork}");
+                    $"Has job: {player.active.hasWork()}");
 
                 embedBuilder.AddField("Pet needs",
                     $"Health: {player.active.health}\n" +
