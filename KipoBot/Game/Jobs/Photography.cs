e7dcf33dd6140c11920aa3f89a1524b152a47a84
OwczarekGit <OwczarekGit>
2021-02-15 19:25:34 +0100

    Set right values for Photography job type.

--- KipoBot/Game/Jobs/Photography.cs
+++ KipoBot/Game/Jobs/Photography.cs
@@ -2,6 +2,7 @@ using System;
 using System.Runtime.CompilerServices;
 using Discord.Commands;
 using KipoBot.Game.Base;
+using KipoBot.Game.Items.Berries;
 using KipoBot.Game.Items.Tools;
 
 namespace KipoBot.Game.Jobs
@@ -13,14 +14,14 @@ namespace KipoBot.Game.Jobs
         {
             name = "Photography";
             reward = 120;
-            //energyCost = 30;
-            //thirstCost = 27;
-            //hungerCost = 20;
-            //happinessCost = 20;
+            energyCost = 30;
+            thirstCost = 27;
+            hungerCost = 20;
+            happinessCost = 20;
             timeDuration = 3;
             xpReward = 40;
             reqStage = Pet.Stage.Baby;
-            setReqItem<Camera>();
+            setRequiredItem<Camera>();
             proceed(ctx);
         }
     }
