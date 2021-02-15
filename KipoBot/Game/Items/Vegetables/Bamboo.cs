8db94b6b23ca89ecc86b6e88f4d1a038ca35a9b0
Ugnė Klimavičiūtė <76807334+UwUgne@users.noreply.github.com>
2021-02-15 17:39:47 +0200

    added Bamboo.cs

---
+++ KipoBot/Game/Items/Vegetables/Bamboo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using KipoBot.Game.Base;
+
+namespace KipoBot.Game.Items.Vegetables
+{
+    [Serializable]
+    class Bamboo : Item
+    {
+        public Beans()
+        {
+            type = Type.Vegetable;
+            price = 50;
+            name = "Beans";
+            description = "best thing for pandas to eat";
+            hunger = 15;
+        }
+    }
+}
