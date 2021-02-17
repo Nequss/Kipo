baea86b3353508fa76f678a8abf7f448b2dda2ef
OwczarekGit <32412218+OwczarekGit@users.noreply.github.com>
2021-02-17 17:46:55 +0100

    Removed useless log informations.

--- KipoBot/Utils/WorkManager.cs
+++ KipoBot/Utils/WorkManager.cs
@@ -21,15 +21,12 @@ namespace KipoBot.Utils
 
                 lock (jobList)
                 {
-                    Program.Logger.info("JobManager: Started job checking...");
                     foreach (var job in jobList)
                     {
-                        Program.Logger.info($"Checking job: {job.name} for pet: {job.worker.name}");
                         
                         if (job.markedForDeletion)
                         {
                             deletionList.Add(job);
-                            Program.Logger.info($"Deleted job for pet: {job.worker.name}");
                         }
                         else
                         {
@@ -37,16 +34,11 @@ namespace KipoBot.Utils
                             {
                                 job.workCompleted();
                                 deletionList.Add(job);
-                                Program.Logger.info("Job completed");
-                            }
-                            else
-                            {
-                                Program.Logger.info($"Job will be completed in: {job.timeEnd.Subtract(currentTime)}");
                             }
                         }
                     }
 
-                    Program.Logger.info("JobManager: Job checking completed!");
+                    Program.Logger.info("JobManager: Job checking completed.");
                     removeCompletedJobs();
 
                     //Thread.Sleep(checkIntervalMinutes*1000*60);
@@ -60,7 +52,6 @@ namespace KipoBot.Utils
         private static void removeCompletedJobs()
         {
             int count = 0;
-            Program.Logger.info("Removing completed jobs...");
             foreach (var job in deletionList)
             {
                 jobList.Remove(job);
@@ -68,7 +59,7 @@ namespace KipoBot.Utils
             }
 
             deletionList = new List<Work>();
-            Program.Logger.info($"Removed {count} completed jobs!");
+            Program.Logger.info($"JobManager: Removed {count} completed jobs.");
         }
     }
 }
