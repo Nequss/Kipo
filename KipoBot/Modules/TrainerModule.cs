e4226cde7a5ad49455a4bbdb88378c8761b47b88
Nequss <snequs@gmail.com>
2021-02-19 03:26:40 +0100

    Added command that lists all abilities

--- KipoBot/Modules/TrainerModule.cs
+++ KipoBot/Modules/TrainerModule.cs
@@ -21,10 +21,40 @@ using KipoBot.Utils;
 
 namespace KipoBot.Modules
 {
+    [Group("t")]
     [Name("trainer")]
     [Summary("Train pettos! Get new abilities for pettos and such!")]
     public class TrainerModule : ModuleBase<SocketCommandContext>
     {
+        private readonly DatabaseService database;
 
+        public TrainerModule(DatabaseService _database)
+        {
+            database = _database;
+        }
+
+        [Command("abilities", RunMode = RunMode.Async)]
+        [Summary("No description")]
+        public async Task Abilities()
+        {
+            Player player = await database.PlayerInfo(Context.User.Id);
+
+            if (player != null)
+            {
+                EmbedBuilder embedBuilder = new EmbedBuilder();
+
+                embedBuilder.Color = Color.Purple;
+
+                foreach (var ability in database.abilities)
+                    embedBuilder.AddField($"{ability.name} | {ability.price} ₭", $"{ability.description}");
+
+                await Context.Channel.SendMessageAsync(embed: embedBuilder.Build());
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync("You are not a member of the Kipo's tamagotchi club.\n" +
+                    "You can join by choosing your first pet, try +help starters");
+            }
+        }
     }
 }
